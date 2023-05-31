1047ebb43867a2003bea507c0883b63fd555888c
Madesh0143 <yellappan0143@gmail.com>
2023-05-31 18:06:08 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "38be5cfceeee744e86a47d09c358bd0f",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
